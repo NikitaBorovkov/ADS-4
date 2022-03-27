7d36e4b01d9cd46f57bd306c23bd460f6f4d36ef
NikitaBorovkov <99392505+NikitaBorovkov@users.noreply.github.com>
2022-03-27 19:43:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -42,5 +42,21 @@ int countPairs2(int *arr, int len, int value) {
   return k;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int last = len;
+  for (int i = len; i < 0; i--) {
+    if (arr[i] > value)
+      last--;
+    else
+      break;
+  }
+  int k = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; j > i + 1; j--) {
+      if (arr[i] + arr[j] == value) 
+        k++;
+      else if (arr[i] + arr[j] < value)
+        break;
+    }
+  }
+  return k;
 }
