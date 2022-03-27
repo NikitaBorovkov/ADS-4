2669eabc1ab1a8c6eabe19cf284643b2603392d2
NikitaBorovkov <99392505+NikitaBorovkov@users.noreply.github.com>
2022-03-27 20:08:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,7 @@
 // Copyright 2021 NNTU-CS
-void sorting(int *arr, int len) {
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = 0; j < len - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int c = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = c;
-      }
-    }
-  }
-}
 int countPairs1(int *arr, int len, int value) {
-  sorting(arr, len);
   int k = 0;
-  for (int i = 0; i < len - 1; i++) {
+  for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         k += 1;
@@ -23,7 +11,6 @@ int countPairs1(int *arr, int len, int value) {
   return k;
 }
 int countPairs2(int *arr, int len, int value) {
-  sorting(arr, len);
   int last = len;
   for (int i = len; i < 0; i--) {
     if (arr[i] > value)
@@ -39,7 +26,6 @@ int countPairs2(int *arr, int len, int value) {
   return k;
 }
 int countPairs3(int *arr, int len, int value) {
-  sorting(arr, len);
   int last = len;
   for (int i = len; i < 0; i--) {
     if (arr[i] > value)
