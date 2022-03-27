1f2df08050ddb0ae3dc6c55f8eec675ce1395ba5
NikitaBorovkov <99392505+NikitaBorovkov@users.noreply.github.com>
2022-03-27 19:44:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -52,7 +52,7 @@ int countPairs3(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; j > i + 1; j--) {
-      if (arr[i] + arr[j] == value) 
+      if (arr[i] + arr[j] == value)
         k++;
       else if (arr[i] + arr[j] < value)
         break;
