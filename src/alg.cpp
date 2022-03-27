30f5045f698362034e6e4478a469bf988a5f60bb
NikitaBorovkov <99392505+NikitaBorovkov@users.noreply.github.com>
2022-03-27 20:17:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,18 +25,34 @@ int countPairs2(int *arr, int len, int value) {
   }
   return k;
 }
-int countPairs3(int *arr, int len, int value) {
-  int last = len;
-  for (int i = len; i < 0; i--) {
-    if (arr[i] > value)
-      last--;
-  }
-  int k = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = len - 1; j > i + 1; j--) {
-      if (arr[i] + arr[j] == value)
-        k++;
+int cbinsearch(int* arr, int left, int len, int value) {
+  int count = 0, i = left, j = len - 1, mid, z;
+  while (i <= j) {
+    mid = i + (j - i) / 2;
+    if (arr[mid] == value && mid != left && mid != len) {
+      count++;
+      z = mid;
+      while (arr[--mid] == value && mid > left) {
+        count++;
+      }
+      while (arr[++z] == value && z < len) {
+        count++;
+      }
+      return count;
+    }
+    else if (arr[mid] > value) {
+      j = mid - 1;
+    }
+    else {
+      i = mid + 1;
     }
   }
-  return k;
+  return 0;;
+}
+int countPairs3(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    count += cbinsearch(arr, i, len, value - arr[i]);
+  }
+  return count;
 }
